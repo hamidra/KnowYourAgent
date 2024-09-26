3fdc6352249b6cf1f018e496a165141d9ef38d6a
hamidra <hamid.alipour@gmail.com>
2024-09-25 21:42:01 -0400

    add did test api

--- app/api/did/route.ts
+++ app/api/did/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
-
+import { authTool } from "@/ai-tools/ai-id";
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams;
   const did = searchParams.get("did");
-  return NextResponse.json({ did });
+  const result = await authTool.invoke({ subject: "ai" });
+  return NextResponse.json(result);
 }
