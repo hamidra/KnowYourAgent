5335e2ed487b39cff5d399afcf2ca79036cb2073
hamidra <hamid.alipour@gmail.com>
2025-01-25 17:11:05 -0500

    add wallet address bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,40 @@
 "use client";
-import { usePrivy } from '@privy-io/react-auth';
+import { usePrivy } from "@privy-io/react-auth";
 
 const Header = () => {
-  const { login, authenticated, logout } = usePrivy();
+  const { login, authenticated, logout, user } = usePrivy();
+  const email = user?.email?.address;
+  const address = user?.wallet?.address;
 
   return (
     <header className="w-full p-4 bg-white shadow-md">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">AI Chat</h1>
+      <div className="container mx-auto flex justify-between items-stretch">
+        <h1 className="text-xl font-bold flex items-center">AI Chat</h1>
         {authenticated ? (
-          <button
-            onClick={logout}
-            className="px-4 py-2 text-white bg-red-600 rounded hover:bg-red-700 transition-colors"
-          >
-            Logout
-          </button>
+          <div className="flex items-stretch gap-4">
+            <div className="flex items-stretch gap-4">
+              <div className="bg-gray-100 rounded-3xl px-4  flex flex-col justify-center">
+                <p className="font-light text-sm max-w-[200px]">
+                  {address
+                    ? address.length > 8
+                      ? `${address.slice(0, 5)}...${address.slice(-5)}`
+                      : address
+                    : "No wallet"}
+                </p>
+                {email && <p className="text-xs text-gray-500">{email}</p>}
+              </div>
+            </div>
+            <button
+              onClick={logout}
+              className="px-4 py-2 rounded-3xl text-white bg-red-600 hover:bg-red-700 transition-colors flex items-center"
+            >
+              Logout
+            </button>
+          </div>
         ) : (
           <button
             onClick={login}
-            className="px-4 py-2 text-white bg-blue-600 rounded hover:bg-blue-700 transition-colors"
+            className="px-4 py-2 text-white rounded-3xl bg-blue-600 rounded hover:bg-blue-700 transition-colors flex items-center"
           >
             Login
           </button>
@@ -28,4 +44,4 @@ const Header = () => {
   );
 };
 
-export default Header; 
+export default Header;
